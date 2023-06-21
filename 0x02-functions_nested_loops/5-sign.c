9ac2aebadd77b748ef356746a6f4fb5c110fddf7
Ansune <ansunesmit1@gmail.com>
2023-06-21 14:52:14 -0700

    5. Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,19 +13,16 @@ int print_sign(int n)
 	if (n > 0)
 	{
 	_putchar('+');
-	_putchar('\n');
 	return (1);
 	}
 	else if (n == 0)
 	{
 	_putchar(48);
-	_putchar('\n');
 	return (0);
 	}
 	else if (n < 0)
 	{
 	_putchar('-');
-	_putchar('\n');
 	return (-1);
 	}
 	return ('n');
