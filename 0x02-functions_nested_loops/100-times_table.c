02c318d048aa2948377b4a5994bef75cc86d4f11
Ansune <ansunesmit1@gmail.com>
2023-06-21 13:31:48 -0700

    13. Nature made the natural numbers; All else is the work of women

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,6 +8,7 @@
 void print_times_table(int n)
 {
 	int i, j, k;
+
 	if (n >= 0 && n <= 15)
 	{
 	for (i = 0; i <= n; i++)
@@ -18,7 +19,7 @@ void print_times_table(int n)
 	if (j == 0)
 	{
 	_putchar(k + '0');
-	} 
+	}
 	else if (k < 10 && j != 0)
 	{
 	_putchar(',');
@@ -26,7 +27,7 @@ void print_times_table(int n)
 	_putchar(' ');
 	_putchar(' ');
 	_putchar(k + '0');
-	} 
+	}
 	else if (k >= 10 && k < 100)
 	{
 	_putchar(',');
@@ -34,7 +35,7 @@ void print_times_table(int n)
 	_putchar(' ');
 	_putchar((k / 10) + '0');
 	_putchar((k % 10) + '0');
-	} 
+	}
 	else if (k >= 100)
 	{
 	_putchar(',');
